d8aea860c4ab09ab63df2ae189c75435c1ad4fa7
Johan Dahlin <johan@gnome.org>
2012-05-28 14:04:34 -0300

    Make it possible to import test_interface without testhelper

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -3,46 +3,46 @@
 import unittest
 
 from gi.repository import GObject
-import testhelper
+try:
+    import testhelper
+except ImportError:
+    testhelper = None
 
 
 GUnknown = GObject.type_from_name("TestUnknown")
 Unknown = GUnknown.pytype
 
 
-class MyUnknown(Unknown, testhelper.Interface):
-    some_property = GObject.Property(type=str)
-
-    def __init__(self):
-        Unknown.__init__(self)
-        self.called = False
-
-    def do_iface_method(self):
-        self.called = True
-        Unknown.do_iface_method(self)
-GObject.type_register(MyUnknown)
-
-
-class MyObject(GObject.GObject, testhelper.Interface):
-    some_property = GObject.Property(type=str)
-
-    def __init__(self):
-        GObject.GObject.__init__(self)
-        self.called = False
-
-    def do_iface_method(self):
-        self.called = True
-GObject.type_register(MyObject)
-
-
 class TestIfaceImpl(unittest.TestCase):
 
     def testReImplementInterface(self):
+        class MyUnknown(Unknown, testhelper.Interface):
+            some_property = GObject.Property(type=str)
+
+            def __init__(self):
+                Unknown.__init__(self)
+                self.called = False
+
+            def do_iface_method(self):
+                self.called = True
+                Unknown.do_iface_method(self)
+        GObject.type_register(MyUnknown)
+
         m = MyUnknown()
         m.iface_method()
         self.assertEqual(m.called, True)
 
     def testImplementInterface(self):
+        class MyObject(GObject.GObject, testhelper.Interface):
+            some_property = GObject.Property(type=str)
+
+            def __init__(self):
+                GObject.GObject.__init__(self)
+                self.called = False
+
+            def do_iface_method(self):
+                self.called = True
+        GObject.type_register(MyObject)
         m = MyObject()
         m.iface_method()
         self.assertEqual(m.called, True)
